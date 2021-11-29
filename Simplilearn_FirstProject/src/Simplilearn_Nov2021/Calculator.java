6dcc227710f92f38042f0da1637b445f2ca02586
Singaravelu Souppourayen <34727666+sing-ideas@users.noreply.github.com>
2021-11-29 21:55:48 +0530

    Simplilearn First COmmit

---
+++ Simplilearn_FirstProject/src/Simplilearn_Nov2021/Calculator.java
@@ -0,0 +1,10 @@
+package Simplilearn_Nov2021;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Simplilearn_FirstProject/src/simplilearnNov2021/typeCasting.java
@@ -0,0 +1,45 @@
+package simplilearnNov2021;
+
+public class typeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//implicit conversion
+	System.out.println("Implicit Type Casting");
+	
+	char a='A';
+	System.out.println("value of a:"+ a);
+	
+	int b=a;
+	System.out.println("Value of b:" +b);
+		
+	float c=a;
+	System.out.println("Value of c:" +c);
+	
+	long d=a;
+System.out.println("Value of d:" + d);
+	
+
+double e=a;
+System.out.println("Value of e:" + e);
+
+
+System.out.println("\n");
+
+
+
+System.out.println("Explicity Type Casting");
+
+//explicit conversion
+
+double x=45.5;
+
+int y=(int)x;
+
+System.out.println("Value of x:" + x);
+System.out.println("value of y:" + y);
+
+	}
+
+}
