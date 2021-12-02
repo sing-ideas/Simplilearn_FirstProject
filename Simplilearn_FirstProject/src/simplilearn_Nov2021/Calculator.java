f7bea55e5907fc8bc6cf446c2362328ccf44bbf6
Singaravelu Souppourayen <34727666+sing-ideas@users.noreply.github.com>
2021-12-02 23:31:43 +0530

    Comment added in the class file

--- Simplilearn_FirstProject/src/simplilearn_Nov2021/Calculator.java
+++ Simplilearn_FirstProject/src/simplilearn_Nov2021/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a =1;
-
+//My first claculator
 System.out.println("VAlue :" + a);
 	}
 
