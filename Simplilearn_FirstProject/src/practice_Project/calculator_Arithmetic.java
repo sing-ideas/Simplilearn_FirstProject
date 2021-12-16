843f611386c819e26d4f30519f0a2fa467691624
Singaravelu Souppourayen <34727666+sing-ideas@users.noreply.github.com>
2021-12-16 14:56:57 +0530

    Learned whileloop and ifelse staementes, not yet run the program

--- Simplilearn_FirstProject/src/practice_Project/calculator_Arithmetic.java
+++ Simplilearn_FirstProject/src/practice_Project/calculator_Arithmetic.java
@@ -6,12 +6,61 @@ public class calculator_Arithmetic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int A; // declaring variable 1
-		int B; // declaring variable 2
-		
-		Scanner Number = new Scanner(System.in);  //creating scanner object here
+		Scanner DataEntry = new Scanner(System.in);
 		
 		
+		while (true)
+		{
+			
+			System.out.println("Addition = 1");
+			System.out.println("Subraction = 2");
+			System.out.println("Multiplication = 3");
+			System.out.println("Division = 4");
+			
+		System.out.println("Select your calculation" );
+		int option = DataEntry.nextInt();	
+		
+		
+		System.out.println("Enter the value ofnumber A:");
+		int A = DataEntry.nextInt(); // declaring variable 1
+		
+		System.out.println("Enter the value ofnumber B:");
+		int B = DataEntry.nextInt(); // declaring variable 2
+		
+		if (option==1)
+			
+		{
+			System.out.println("Value of " + A + "+" + B + "=" + (A+B));
+			
+		}
+		else 
+		
+			if (option==2)
+				
+			{
+				System.out.println("Value of " + A + "+" + B + "=" + (A+B));
+				
+			}
+			else if (option==1)
+				
+			{
+				System.out.println("Value of " + A + "+" + B + "=" + (A+B));
+				
+			}
+			else if (option==3)
+			
+		{
+			System.out.println("Value of " + A + "+" + B + "=" + (A+B));
+			
+		}
+		else
+			
+		{
+		System.out.println("Thank you for using the app");
+		
+		}
+				
+		} //while loop ends here
 		
 	}
 
